6862286e02a34c9631c5f4db998a8103b03539c6
TomDenem1010 <43414435+TomDenem1010@users.noreply.github.com>
2026-03-02 17:12:05 +0100

    NEW: init

---
+++ src/main/java/com/home/hastur/HasturApplication.java
@@ -0,0 +1,13 @@
+package com.home.hastur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HasturApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HasturApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/home/hastur/HasturApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.hastur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HasturApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
