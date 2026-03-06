027cf5eaeb18fd9d953ade7bc9112a0080e21868
TomDenem1010 <43414435+TomDenem1010@users.noreply.github.com>
2026-03-06 15:03:40 +0100

    NEW: aws-load-balancer-cross-zone-load-balancing-enabled

--- src/main/java/com/home/carcosa/usermanagement/dto/User.java
+++ src/main/java/com/home/carcosa/usermanagement/dto/User.java
@@ -1,7 +1,8 @@
 package com.home.carcosa.usermanagement.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.home.carcosa.usermanagement.Role;
 
-public record User(String id, String name, String password, Role role) {
+public record User(String id, String name, @JsonIgnore String password, Role role) {
 
 }
